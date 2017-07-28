e1655b8fd78739797c8c5637417b792665524903
Pitipat Srichairat <pitipat.dop@gmail.com>
2017-07-28 12:51:00 +0700

    add mongo

--- app/index.js
+++ app/index.js
@@ -1,10 +1,31 @@
 const bodyParser = require('body-parser');
 const express = require('express');
+const session = require('express-session');
+const dotenv = require('dotenv');
+const path = require('path');
+const mongoose = require('mongoose');
+const chalk = require('chalk');
+
+dotenv.config();
 
 const app = express();
+
+/**
+ * Connect to MongoDB.
+ */
+mongoose.Promise = global.Promise;
+mongoose.connect(process.env.MONGODB_URI || process.env.MONGOLAB_URI, {
+  useMongoClient: true,
+});
+mongoose.connection.on('error', () => {
+  console.log('%s MongoDB connection error. Please make sure MongoDB is running.', chalk.red('✗'));
+  process.exit();
+});
+
 app.set('port', process.env.PORT || 3000);
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
+
 
 const apiPrefix = '/v1';
 
